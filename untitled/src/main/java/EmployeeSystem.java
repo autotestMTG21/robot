388c44eafb2992ef1e6377fde0a144cca699c405
ZhengL <zheng.lin@iths.se>
2022-02-17 22:20:47 +0100

    Red

--- untitled/src/main/java/EmployeeSystem.java
+++ untitled/src/main/java/EmployeeSystem.java
@@ -1,2 +1,9 @@
+import java.util.ArrayList;
+
 public class EmployeeSystem {
+    private ArrayList employeelist = null;
+
+    public EmployeeSystem(){
+        employeelist = new ArrayList();
+    }
 }
--- untitled/src/test/java/EmployeeSystemTest.java
+++ untitled/src/test/java/EmployeeSystemTest.java
@@ -1,3 +1,44 @@
+import org.junit.jupiter.api.*;
+
 public class EmployeeSystemTest {
     EmployeeSystem testObject;
+
+    @BeforeEach
+    public void beforeEach(){
+        testObject = new EmployeeSystem();
+        System.out.println("This happens before each test in this class");
+    }
+
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("This happens before all tests in this class");
+    }
+
+    @AfterEach
+    public void afterEach(){
+        System.out.println("This happens after each test in this class");
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("This is the last thing that is run before the test class is finished");
+    }
+
+    @Test
+    public void returnAddEmployee(){
+
+        testObject.addEmployee(Employee employee);
+
+
+
+    }
+
+    @Test
+    public void returnRemoveEmployee(){
+
+
+    }
+
+
+
 }
